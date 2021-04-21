66c6cbfb774f6247cc396babc7d62039492a6c01
Anuki-UCSC <65793111+Anuki-UCSC@users.noreply.github.com>
2021-04-21 12:48:51 +0530

    Neural network linear

---
+++ NN_linear/linear.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import pandas as pd
+import numpy as np
+
+train_df= pd.read_csv('train.csv')
+np.random.shuffle(train_df.values)
+
+print(train_df.head(5))
+
+# print(train_df.x.values[0:5])
+# print(type(train_df.x.values))
+
+model =keras.Sequential([
+    keras.layers.Dense(4,input_shape=(2,),activation='relu'),
+    keras.layers.Dense(2,activation='sigmoid')])
+
+model.compile(optimizer='adam',
+              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+x=np.column_stack((train_df.x.values,train_df.y.values))
+
+model.fit(x,train_df.color.values ,batch_size=4)
