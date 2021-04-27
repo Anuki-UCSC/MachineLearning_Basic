3f1b28d2a94cc45e22f64fb6028530b4087084b5
Anuki-UCSC <alwis.anuki@gmail.com>
2021-04-27 23:05:49 +0530

    single & mmultilevel inheritance

---
+++ oop with python/demo7-single_or_multiLevel_inheritance.py
@@ -0,0 +1,30 @@
+class A:
+    def feature1(self):
+        print("Feature 1 is working!")
+
+    def feature2(self):
+        print("Feature 2 is working!")
+
+class B(A): #this is how inheritance show in python - B is inherited by A
+    def feature3(self):
+        print("Feature 3 is working!")
+
+    def feature4(self):
+        print("Feature 4 is working!")
+
+class C(B):
+    def feature5(self):
+        print("Feature 5 is working!")
+
+
+a1= A()
+a1.feature1()
+
+b1=B()
+b1.feature1()
+b1.feature3()
+
+c1=C()
+c1.feature1()
+c1.feature3()
+c1.feature5()
