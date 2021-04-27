64dbd3350e8b3db4d920e946cb243e17c9c916e0
Anuki-UCSC <alwis.anuki@gmail.com>
2021-04-27 22:09:22 +0530

    Laptop class - inner class

--- oop with python/demo6-class_inside_a_class.py
+++ oop with python/demo6-class_inside_a_class.py
@@ -4,6 +4,16 @@ class Student:
     def __init__(self,name,rollno):
         self.name=name
         self.rollno=rollno
+        self.lap=self.Laptop() # call an inner class inside the constructor of outer class
+
+
+
+    class Laptop:
+        def __init__(self):
+            self.brand="HP"
+            self.processer="i7"
+            self.ram=8
+
 
 s1=Student("Anuki",2)
 s2=Student("Sanduni",5)
