cfca864746d219a64809a58cae51a5e874a6edc6
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-12 23:48:49 +0530

    SVM init method

---
+++ SVM-support Vector Machine/svm.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class SVM:
+    def __init__(self, lr=0.001, lambda_param=0.01,n_iters=1000):
+        self.lr=lr
+        self.lmbp=lambda_param
+        self.n_iters=n_iters
+        self.w=None
+        self.b=None
+
+
