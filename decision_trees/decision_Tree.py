268e07477d1da9d764b29b73aad0699b8ba7ee0d
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-15 23:32:09 +0530

    init method of node

--- decision_trees/decision_Tree.py
+++ decision_trees/decision_Tree.py
@@ -7,4 +7,12 @@ def entropy(y):
     ps=hist/len(y)
     return -np.sum([p * np.log2(p) for p in ps if p >0])
 
-     
+
+class Node:
+    def __init__(self,feature=None,threshod=None, left=None, right=None,*,value=None):
+        self.feature=feature
+        self.threshold=threshod
+        self.left=left
+        self.right=right
+        self.value=value
+        
