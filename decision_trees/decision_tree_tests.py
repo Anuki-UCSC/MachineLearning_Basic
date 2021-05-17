15f68722f542243d3ee1ad7a3f8d860d5eeb9e36
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-17 19:21:02 +0530

    split data in to train , test sets

--- decision_trees/decision_tree_tests.py
+++ decision_trees/decision_tree_tests.py
@@ -7,3 +7,11 @@ from decision_tree import DecisionTree
 def accuracy(y_true, y_pred):
     accuracy=np.sum(y_true==y_pred)/len(y_true)
     return accuracy
+
+data=datasets.load_breast_cancer()
+X=data.data
+y=data.target
+
+X_train,X_test,y_train,y_test= train_test_split(X ,y, test_size=0.2, random_state=1234)
+
+
