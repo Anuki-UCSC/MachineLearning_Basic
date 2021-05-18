de9fbb25fb0e21fea7f552aaec555ba77e3e8978
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-18 23:51:13 +0530

    train test data produced

--- Random_Forest/random_forest_tests.py
+++ Random_Forest/random_forest_tests.py
@@ -8,3 +8,9 @@ def accuracy(y_true, y_pred):
     accuracy=np.sum(y_true==y_pred) / len(y_true)
     return accuracy
 
+data =datasets.load_breast_cancer()
+X=data.data
+y=data.target
+
+X_train, X_test, y_train, y_test= train_test_split(X,y, test_size=0.2, random_state=1234)
+
