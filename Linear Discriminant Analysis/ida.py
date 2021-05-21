814245d3184a21de0097d7fcae7849f2aeb3998d
Anuki-UCSC <alwis.anuki@gmail.com>
2021-05-21 23:58:50 +0530

    fit

--- Linear Discriminant Analysis/ida.py
+++ Linear Discriminant Analysis/ida.py
@@ -6,4 +6,8 @@ class LDA:
         self.n_components=n_components
         self.linear_discriminants=None
 
-        
+    def fit(self,X,y):
+        n_features=X.shape[1]
+        class_labals=np.unique(y)
+
+       
