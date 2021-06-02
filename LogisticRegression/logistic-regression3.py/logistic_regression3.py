a98b74d0dbbe9ce9db33cc16cbfd8f1a8be05c90
Anuki-UCSC <alwis.anuki@gmail.com>
2021-06-02 21:39:20 +0530

    logistic

---
+++ LogisticRegression/logistic-regression3.py/logistic_regression3.py
@@ -0,0 +1,21 @@
+from sklearn, datasets import make_classification
+from matplotlib import pyplot as plt 
+from sklearn.lenear_model import LogisticRegression
+From sklearn.model_selection import train_testsplit
+from sklearn.metrix import confuion_matrix
+import pandas as pd
+
+x,y=make+classification{
+    n_samples=100,
+    n_samples=1000,
+    n_classes = 2,
+    n_cluster =perclass,
+    flip_y ==0.01,
+    n_information =1,
+    n_repeated=0.n_classes
+    
+
+
+}
+
+
